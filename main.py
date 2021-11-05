3217d07d31f2538e3e0f12b3813aaab8e75e49ea
dokieki <hotboii4721@protonmail.com>
2021-11-05 21:30:53 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from json import load
+
+app = Flask(__name__);
+
+@app.route('/')
+def index():
+	data = load(open('data.json', 'r'));
+
+	return render_template('index.html',
+		contacts = data['contacts'],
+		projects = data['projects'],
+		music = data['music'],
+		love = data['love']
+	);
+
+@app.route('/lite')
+def lite():
+	return render_template('lite.html');
+
+app.run(port = 5287, host = 'localhost', debug = True);
